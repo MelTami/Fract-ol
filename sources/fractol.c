227bd7fc8e3e1568a20ae56fd6b336b2da6f11ea
mvavasso <mvavasso@student.42sp.org.br>
2022-09-01 18:33:21 +0200

    pixel print

--- sources/fractol.c
+++ sources/fractol.c
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fractol.h                                          :+:      :+:    :+:   */
+/*   fractol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mvavasso <mvavasso@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/09 20:41:59 by mvavasso          #+#    #+#             */
-/*   Updated: 2022/09/01 18:03:15 by mvavasso         ###   ########.fr       */
+/*   Created: 2022/08/16 01:09:33 by mvavasso          #+#    #+#             */
+/*   Updated: 2022/08/16 01:09:34 by mvavasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRACTOL_H
-# define FRACTOL_H
-
-# include "../Libft/libft.h"
-# include <mlx.h>
-
-
-typedef struct	s_data {
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
-
-#endif
--- sources/main.c
+++ sources/main.c
@@ -6,13 +6,31 @@
 /*   By: mvavasso <mvavasso@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 20:39:10 by mvavasso          #+#    #+#             */
-/*   Updated: 2022/08/09 20:45:17 by mvavasso         ###   ########.fr       */
+/*   Updated: 2022/09/01 18:21:23 by mvavasso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
-int main(void)
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
-	ft_putstr_fd("Banana\n", 1);
-	return (0);
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1000, 700, "Fractol");
+	img.img = mlx_new_image(mlx, 1000, 700);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 10, 10);
+	mlx_loop(mlx);
 }
